11e7d628cffe28e56d7de5e1d817705abd822791
Athelia Crosmun <hi@athelia.codes>
2021-07-03 10:04:03 -0400

    Configure crlf in editorconfig; completed largest-subarray and binary helper fn

---
+++ largest-subarray.py
@@ -0,0 +1,19 @@
+import pdb
+def largest_seq(lst):
+    my_dict = {}
+    keys_list = []
+    for i in range(len(lst)):
+        if i == 0:
+            my_dict[(0,0)] = lst[0]
+            keys_to_add = [(0,0)]
+        else:
+            keys_to_add = []
+            for key in keys_list:
+                # pdb.set_trace()
+                my_dict[(key[0], i)] = my_dict[key] + lst[i]
+                keys_to_add.append((key[0], i))
+            my_dict[(i,i)] = lst[i]
+            keys_to_add.append((i,i))
+        keys_list += keys_to_add
+    print( my_dict)
+largest_seq([1, 0, 3, -8, 4, -2, 3])
---
+++ make-binary.py
@@ -0,0 +1,19 @@
+def make_binary(n):
+    """Given a positive integer, return its binary representation as a list
+    >>> 4
+    [1, 0, 0]
+    >>> 1
+    [1]
+    >>> 15
+    [1, 1, 1, 1]
+    """
+    result = []
+    while n > 0:
+        result.append(n % 2)
+        n = n // 2
+    result.reverse()
+    return result
+
+print(make_binary(4) == [1, 0, 0])
+print(make_binary(1) == [1])
+print(make_binary(15) == [1, 1, 1, 1])
