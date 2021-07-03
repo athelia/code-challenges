73d9c34a3f066fc25476c8b5927b2a21f5282651
Athelia Crosmun <hi@athelia.codes>
2021-07-03 13:38:55 -0400

    Solve Euler 3

--- project-euler.py
+++ project-euler.py
@@ -24,15 +24,29 @@ def sum_even_fib(ceiling):
             total += fibonacci(i)
     return total
 
-print(sum_even_fib(32))
+# print(sum_even_fib(32))
 
 def is_prime(x):
-    for i in range(x//2):
-        pass
+    if x == 2 or x == 3 or x == 5 or x == 7 or x == 11:
+        return True
+    if x < 2 or x % 2 == 0 or x % 3 == 0:
+        return False
+    factor = 5
+    #Primes are of the form 6n +/- 1. Test 5 and 5+2 (7), 5+6 (11) and 5+6+2 (13), etc.
+    while factor <= x // 2 + 1:
+        # print('/t',factor)
+        if x % factor == 0 or x % (factor + 2) == 0:
+            return False
+        factor += 6
+    return True
+
+# print(is_prime(5003))
 
 def largest_prime_factor(n):
-    for i in range(n/2):
+    for i in range(2, n//2 + 1):
         if n % i == 0:
             factor = n / i
             if is_prime(factor):
                 return factor
+
+print(largest_prime_factor(600851475143))
