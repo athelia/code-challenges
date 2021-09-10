7d178d771c625b449bb82db662efa40c05e42a89
athelia <athelia.hk@gmail.com>
2021-09-10 18:51:45 -0400

    Modify comments; await sleep

--- asyncrand.py
+++ asyncrand.py
@@ -14,8 +14,8 @@ async def make_random(idx: int, threshold: int = 6) -> int:
     i = random.randint(0, 10)
     while i <= threshold:
         print(c[idx + 1] + f'makerandom({idx})=={i} too low; retrying after {idx + 1} seconds.')
-        # await asyncio.sleep(idx + 1)
-        asyncio.sleep(idx + 1)
+        await asyncio.sleep(idx + 1)
+        # asyncio.sleep(idx + 1)
         i = random.randint(0, 10)
     print(c[idx + 1] + f'-------> Finished: makerandom({idx})=={i}' + c[0])
     return i
@@ -25,13 +25,13 @@ async def main():
     res = await asyncio.gather((make_random(0, 9)), (make_random(1, 8)), (make_random(2, 7)))
     # res -> variable to hold result
     # await -> required to get the result from coroutines
-    # asyncio.gather -> async controller
+    # asyncio.gather -> async controller/event loop/coordinator
     # (*(make_random())) -> * for unpacking; ?
     # make_random(i, 10 - i - 1) for i in range(3) -> ternary for loop for 3
     # function calls 
     return res
 
 if __name__ == '__main__':
-    # random.seed(444)
+    random.seed(444)
     r1, r2, r3 = asyncio.run(main())
     print(f'\nr1: {r1}, r2: {r2}, r3: {r3}')
