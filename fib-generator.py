9a6e457ae5541ed56cae2b0a272eef83a160f585
athelia <athelia.hk@gmail.com>
2021-09-10 18:54:51 -0400

    Practice file with generator/yield

---
+++ fib-generator.py
@@ -0,0 +1,12 @@
+def fibonacci():
+    a, b = 0, 1
+    while True:
+        a, b = b, a+b
+        yield b
+
+if __name__ == '__main__':
+    f = fibonacci()
+    print('Initial: 0, 1')
+    for _ in range(10):
+        print(f.__next__())
+    
