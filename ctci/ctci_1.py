ccb932b5c5f0451e9a64c138a9c879900e93113a
Athelia Crosmun <hi@athelia.codes>
2023-06-08 16:23:29 -0400

    CTCI 1.6; test and debug 1.1 and 1.4

--- ctci/ctci_1.py
+++ ctci/ctci_1.py
@@ -14,11 +14,12 @@ def is_unique_simple(string: str) -> bool:
 
 
 def is_unique_sorted_string(string: str) -> bool:
-    """ Use a sorted string for a compromise between space and runtime. O(n * log n) runtime, O(n) space"""
+    """ Use a sorted string as an alternative. O(n * log n) runtime, O(n) space"""
     if len(string) < 2:
         return True
-    for i, char in enumerate(sorted(string[1:])):
-        if string[i+1] == char:
+    sorted_string = sorted(string)
+    for i in range(len(sorted_string) - 1):
+        if sorted_string[i+1] == sorted_string[i]:
             return False
     return True
 
@@ -26,7 +27,7 @@ def is_unique_sorted_string(string: str) -> bool:
 def is_unique_no_additional_data_structures(string: str) -> bool:
     """ Use a nested loop for best space. O(n^2) runtime, O(1) space"""
     for i in range(len(string) - 1):
-        for j in range(i, len(string)):
+        for j in range(i + 1, len(string)):
             if string[i] == string[j]:
                 return False
     return True
@@ -42,10 +43,13 @@ def is_unique_no_additional_data_structures(string: str) -> bool:
 def is_anagram_of_palindrome_hashmap(string: str) -> bool:
     counts = {}
     for char in string:
-        counts[char] += counts.get(char, 0)
+        if char in ([" ", ".", ",", "!", "-", "_", ":", ";", "?"]):
+            continue
+        else:
+            counts[char.lower()] = counts.get(char.lower(), 0) + 1
     odd_count_seen = False
-    for count in counts:
-        if count % 2 == 1:
+    for letter in counts:
+        if counts[letter] % 2 == 1:
             if odd_count_seen:
                 return False
             else:
@@ -53,5 +57,42 @@ def is_anagram_of_palindrome_hashmap(string: str) -> bool:
     return True
 
 
-def is_anagram_of_palindrome(string: str) -> bool:
-    pass
+# 1.6 String Compression: Implement a method to perform basic string compression using the counts
+# of repeated characters. For example, the string aabcccccaaa would become a2b1c5a3. If the
+# "compressed" string would not become smaller than the original string, your method should return
+# the original string. You can assume the string has only uppercase and lowercase letters (a - z).
+
+def compress_string(string: str) -> str:
+    result = ""
+    current_count = 1
+    for i in range(len(string)):
+        if i == len(string) - 1:
+            result += string[i] + str(current_count)
+        elif string[i + 1] == string[i]:
+            current_count += 1
+        else:
+            result += string[i] + str(current_count)
+            current_count = 1
+    return string if len(string) < len(result) else result
+
+
+if __name__ == "__main__":
+    print("\n\nCTCI 1.1")
+    print(f"Testing is_unique_simple('')={is_unique_simple('')}")
+    print(f"Testing is_unique_simple('abcd')={is_unique_simple('abcd')}")
+    print(f"Testing is_unique_simple('aba')={is_unique_simple('aba')}")
+    print(f"Testing is_unique_no_additional_data_structures('')={is_unique_no_additional_data_structures('')}")
+    print(f"Testing is_unique_no_additional_data_structures('abcd')={is_unique_no_additional_data_structures('abcd')}")
+    print(f"Testing is_unique_no_additional_data_structures('aba')={is_unique_no_additional_data_structures('aba')}")
+    print(f"Testing is_unique_sorted_string('')={is_unique_sorted_string('')}")
+    print(f"Testing is_unique_sorted_string('abcd')={is_unique_sorted_string('abcd')}")
+    print(f"Testing is_unique_sorted_string('aba')={is_unique_sorted_string('aba')}")
+    print("\n\nCTCI 1.4")
+    print(f"Testing is_anagram_of_palindrome_hashmap('')={is_anagram_of_palindrome_hashmap('')}")
+    print(f"Testing is_anagram_of_palindrome_hashmap('tactcoa')={is_anagram_of_palindrome_hashmap('tactcoa')}")
+    print(f"Testing is_anagram_of_palindrome_hashmap('Tact Coa')={is_anagram_of_palindrome_hashmap('Tact Coa')}")
+    print("\n\nCTCI 1.6")
+    print(f"Testing compress_string('')={compress_string('')}")
+    print(f"Testing compress_string('aabcccccaaa')={compress_string('aabcccccaaa')}")
+    print(f"Testing compress_string('abcde')={compress_string('abcde')}")
+
